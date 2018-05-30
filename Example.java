554032328828ef3976ea5663db7148dbfa9ad01d
osmanseytablaev <osman.seytablaev@gmail.com>
2018-05-30 20:31:20 +0300

    My first commit

---
+++ Example.java
@@ -0,0 +1,9 @@
+/*Это пример простой программы на Java.
+Присвойте файлу с исходным кодом имя Example.java.*/
+class Example {
+// Выполнение любой программы на Java начинается
+// с вызова метода main()
+public static void main(String args[]) {
+System.out.println("Java правит Интернетом!");
+   }
+}
