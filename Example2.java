a8cae37f0d405445299abff4ebad06bc16dcd974
osmanseytablaev <osman.seytablaev@gmail.com>
2018-06-01 10:43:33 +0300

    GalToLit

---
+++ Example2.java
@@ -0,0 +1,11 @@
+class Example2 {
+public static void main(String args[]) {
+int var1; // объявляется переменная '4-------Объявпенне переменных
+int var2; // объявляется еще одна переменная
+var1 = 1024; // переменной varl присваивается значение 1024 �4-----1
+System.out.println("Пepeмeннaя var1 содержит " + var1);
+var2 = var1 / 2;
+System.out.print("Пepeмeннaя var2 содержит var1 / 2: ");
+System.out.println(var2);
+   }
+}
