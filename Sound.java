fefbb17ab0611a883936701211d512d5856d2e1a
osmanseytablaev <osman.seytablaev@gmail.com>
2018-06-05 09:19:54 +0300

    Sound

---
+++ Sound.java
@@ -0,0 +1,7 @@
+class Sound {
+	public static void main(String args []) {
+	double dist;
+	dist = 1100 * 7.2 ;
+	System.out.println("Paccтoяниe до места вспышки молнии " +"составляет " + dist + " футов");
+        }
+      }
