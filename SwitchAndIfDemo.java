47afd207a95888334da7f4a48c8994199c69877b
osmanseytablaev <osman.seytablaev@gmail.com>
2018-06-08 15:11:42 +0300

    Smitch

---
+++ SwitchAndIfDemo.java
@@ -0,0 +1,26 @@
+class SwitchAndIfDemo {
+	public static void main(String args[]) {
+		String fruit = "pineapple";
+		switch(fruit) {
+			case "apricot":
+				System.out.println("apricot");
+				break;
+			case "apple":
+				System.out.println("apple");
+				break;
+			case "orange":
+				System.out.println("orange");
+				break;
+			case "kivi":
+				System.out.println("kivi");
+				break;
+			case "pineapple":
+				System.out.println("pineapple");
+				break;
+			default:
+				System.out.println("No matches");
+				break;
+		}
+     	  }					
+}
+ 
