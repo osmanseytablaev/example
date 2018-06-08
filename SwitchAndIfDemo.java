44faee37b10f2696a125530cf432b80bd4d2edc0
osmanseytablaev <osman.seytablaev@gmail.com>
2018-06-08 16:06:09 +0300

    switch and if example

--- SwitchAndIfDemo.java
+++ SwitchAndIfDemo.java
@@ -1,5 +1,5 @@
 class SwitchAndIfDemo {
-	public static void main(String args[]) {
+	public static void main(String artags[]) {
 		String fruit = "pineapple";
 		switch(fruit) {
 			case "apricot":
@@ -20,7 +20,20 @@ class SwitchAndIfDemo {
 			default:
 				System.out.println("No matches");
 				break;
-		}
+		 	}
+
+			if (fruit == "apricot") {
+				System.out.println("apricot");
+			} else if (fruit == "apple") {
+				System.out.println("apple");
+			} else if (fruit == "orange") {
+			        System.out.println("orange");
+			} else if (fruit == "kivi") {
+				System.out.println("kivi");
+			} else if (fruit == "pineapple") {
+				System.out.println("pineapple");
+			} else {
+				System.out.println("No matches");
+			}
      	  }					
 }
- 
